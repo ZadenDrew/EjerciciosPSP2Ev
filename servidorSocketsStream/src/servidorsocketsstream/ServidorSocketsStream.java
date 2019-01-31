98d8e29d3817c3a4dd4e326d50cfc5f76dcc4b16
Andrea <andrea@Drew>
2019-01-31 02:01:19 +0100

    ejemplo

---
+++ servidorSocketsStream/src/servidorsocketsstream/ServidorSocketsStream.java
@@ -0,0 +1,58 @@
+package servidorsocketsstream;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author andrea
+ */
+public class ServidorSocketsStream {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.println("Creando socket servidor");
+
+            ServerSocket serverSocket = new ServerSocket();
+
+            System.out.println("Realizando el bind");
+
+            InetSocketAddress addr = new InetSocketAddress("locahost", 5555);
+            serverSocket.bind(addr);
+
+            System.out.println("Aceptando conexiones");
+
+            Socket newSocket = serverSocket.accept();
+
+            System.out.println("Conexi�n recibida");
+
+            InputStream is = newSocket.getInputStream();
+            OutputStream os = newSocket.getOutputStream();
+
+            byte[] mensaje = new byte[25];
+            is.read(mensaje);
+
+            System.out.println("Mensaje recibido: " + new String(mensaje));
+
+            System.out.println("Cerrando el nuevo socket");
+
+            newSocket.close();
+
+            System.out.println("Cerrando el socket servidor");
+
+            serverSocket.close();
+
+            System.out.println("Terminado");
+
+        } catch (IOException e) {
+        }
+    }
+
+}
