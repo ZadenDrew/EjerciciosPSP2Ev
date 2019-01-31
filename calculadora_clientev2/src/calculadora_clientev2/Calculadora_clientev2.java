da1f4c7cffa5a410d339f18490d9ad52dab0ac40
Andrea <andrea@Drew>
2019-01-31 01:51:42 +0100

    V2 sin terminar

---
+++ calculadora_clientev2/src/calculadora_clientev2/Calculadora_clientev2.java
@@ -0,0 +1,17 @@
+package calculadora_clientev2;
+
+/**
+ *
+ * @author andrea
+ */
+public class Calculadora_clientev2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora cal = new Calculadora();
+        cal.setVisible(true);
+    }
+
+}
