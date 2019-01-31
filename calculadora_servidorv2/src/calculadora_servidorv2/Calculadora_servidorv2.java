83305310df67604a2eb3ba6222c71357a2921553
Andrea <andrea@Drew>
2019-01-31 01:52:22 +0100

    V2 sin terminar

---
+++ calculadora_servidorv2/src/calculadora_servidorv2/Calculadora_servidorv2.java
@@ -0,0 +1,17 @@
+package calculadora_servidorv2;
+
+/**
+ *
+ * @author andrea
+ */
+public class Calculadora_servidorv2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Servidor ser = new Servidor();
+        ser.setVisible(true);
+
+    }
+}
